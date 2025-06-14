7dbc29995a5085f002d81a25c14c04b813a0a76b
Minsup Lee <m1nsuppp@gmail.com>
2025-06-14 23:54:49 +0900

    feat(web): transformer

--- apps/web/app/_components/t-shirt-area-impl.tsx
+++ apps/web/app/_components/t-shirt-area-impl.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { useSuspenseQuery } from '@tanstack/react-query';
-import type { JSX } from 'react';
-import { Layer, Stage, Image } from 'react-konva';
+import { useRef, type JSX } from 'react';
+import { Layer, Stage, Image, Transformer } from 'react-konva';
 import { loadImage } from '../../lib/load-image';
 import { useTShirtSide } from '../_hooks/use-t-shirt-side';
 import type { TShirtSide } from '../../models/t-shirt.model';
+import type Konva from 'konva';
 
 function updateCursor(action: 'over' | 'out'): void {
   document.body.style.cursor = action === 'over' ? 'pointer' : 'default';
@@ -27,6 +28,9 @@ export function TShirtAreaImpl(): JSX.Element {
 
   const { innerWidth: stageWidth, innerHeight: stageHeight } = window;
 
+  const imageRef = useRef<Konva.Image>(null);
+  const transformerRef = useRef<Konva.Transformer>(null);
+
   return (
     <Stage
       width={stageWidth}
@@ -34,17 +38,26 @@ export function TShirtAreaImpl(): JSX.Element {
     >
       <Layer>
         <Image
+          ref={imageRef}
           image={image}
           x={stageWidth / 2 - image.width / 2}
           y={stageHeight / 2 - image.height / 2}
           width={image.width}
           height={image.height}
           draggable
-          onPointerOver={() => {
+          onPointerOver={(e) => {
             updateCursor('over');
           }}
           onPointerOut={() => {
             updateCursor('out');
+          }}
+        />
+        <Transformer
+          ref={(transformer) => {
+            if (transformer !== null && imageRef.current !== null) {
+              transformerRef.current = transformer;
+              transformerRef.current.nodes([imageRef.current]);
+            }
           }}
         />
       </Layer>
